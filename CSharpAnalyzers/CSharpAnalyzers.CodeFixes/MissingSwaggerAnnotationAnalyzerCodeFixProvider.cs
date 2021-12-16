d3fa03acaa94ad818afb1fff635cc7d0e210bad4
tjthrill <terryhammett31@gmail.com>
2021-12-15 23:46:20 -0600

    Initial setup for repo. Added first iteration of MissingSwaggerAnnotationsAnalyzer.

---
+++ CSharpAnalyzers/CSharpAnalyzers.CodeFixes/MissingSwaggerAnnotationAnalyzerCodeFixProvider.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Collections.Immutable;
+using System.Composition;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CSharpAnalyzers
+{
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(MissingSwaggerAnnotationAnalyzerCodeFixProvider)), Shared]
+    public class MissingSwaggerAnnotationAnalyzerCodeFixProvider : CodeFixProvider
+    {
+        public sealed override ImmutableArray<string> FixableDiagnosticIds
+        {
+            get { return ImmutableArray.Create(MissingSwaggerAnnotationsAnalyzer.DiagnosticId); }
+        }
+
+        public sealed override FixAllProvider GetFixAllProvider()
+        {
+            // See https://github.com/dotnet/roslyn/blob/main/docs/analyzers/FixAllProvider.md for more information on Fix All Providers
+            return WellKnownFixAllProviders.BatchFixer;
+        }
+
+        public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
+        {
+            var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
+
+            var diagnostic = context.Diagnostics.First();
+            var diagnosticSpan = diagnostic.Location.SourceSpan;
+
+            // Find the type declaration identified by the diagnostic.
+            var declaration = root.FindToken(diagnosticSpan.Start).Parent.AncestorsAndSelf().OfType<MethodDeclarationSyntax>().First();
+
+            // Register a code action that will invoke the fix.
+            context.RegisterCodeFix(
+                CodeAction.Create(
+                    title: CodeFixResources.CodeFixTitle,
+                    createChangedDocument: c => AddSwaggerAnnotation(context.Document, declaration, root, c),
+                    equivalenceKey: nameof(CodeFixResources.CodeFixTitle)),
+                diagnostic);
+        }
+
+        private Task<Document> AddSwaggerAnnotation(Document document, MethodDeclarationSyntax methodDeclaration, SyntaxNode root, CancellationToken cancellationToken)
+        {
+            var attributeListSyntaxes = SyntaxFactory.AttributeList();
+
+            attributeListSyntaxes = attributeListSyntaxes.AddAttributes(
+                SyntaxFactory.Attribute(
+                    SyntaxFactory.ParseName("ProducesResponseType"),
+                    SyntaxFactory.AttributeArgumentList(
+                        SyntaxFactory.SeparatedList<AttributeArgumentSyntax>()
+                            .Add(SyntaxFactory.AttributeArgument(SyntaxFactory.ParseExpression("Microsoft.AspNetCore.Http.StatusCodes.Status200OK")))
+                            .Add(SyntaxFactory.AttributeArgument(SyntaxFactory.ParseExpression($"Type = typeof({MissingSwaggerAnnotationsAnalyzer.ConvertTypeNameToSwaggerTypeName(methodDeclaration.ReturnType.ToString())})")))
+                    )
+                )
+            );
+
+            var newMethodDeclaration = SyntaxFactory.MethodDeclaration(
+                methodDeclaration.AttributeLists.Add(attributeListSyntaxes),
+                methodDeclaration.Modifiers,
+                methodDeclaration.ReturnType,
+                methodDeclaration.ExplicitInterfaceSpecifier,
+                methodDeclaration.Identifier,
+                methodDeclaration.TypeParameterList,
+                methodDeclaration.ParameterList,
+                methodDeclaration.ConstraintClauses,
+                methodDeclaration.Body,
+                methodDeclaration.ExpressionBody,
+                methodDeclaration.SemicolonToken
+           );
+
+            var newRoot = root.ReplaceNode(methodDeclaration, newMethodDeclaration);
+
+            return Task.FromResult(document.WithSyntaxRoot(newRoot));
+        }
+    }
+}
---
+++ CSharpAnalyzers/CSharpAnalyzers/MissingSwaggerAnnotationsAnalyzer.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+
+namespace CSharpAnalyzers
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class MissingSwaggerAnnotationsAnalyzer : DiagnosticAnalyzer
+    {
+        public const string DiagnosticId = "MissingSwaggerAnnotations";
+
+        // You can change these strings in the Resources.resx file. If you do not want your analyzer to be localize-able, you can use regular strings for Title and MessageFormat.
+        // See https://github.com/dotnet/roslyn/blob/main/docs/analyzers/Localizing%20Analyzers.md for more on localization
+        private static readonly LocalizableString Title = new LocalizableResourceString(nameof(Resources.SwaggerAnalyzerTitle), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString MessageFormat = new LocalizableResourceString(nameof(Resources.SwaggerAnalyzerMessageFormat), Resources.ResourceManager, typeof(Resources));
+        private static readonly LocalizableString Description = new LocalizableResourceString(nameof(Resources.SwaggerAnalyzerDescription), Resources.ResourceManager, typeof(Resources));
+        private const string Category = "Swagger";
+
+        private static readonly DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, MessageFormat, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Description);
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get { return ImmutableArray.Create(Rule); } }
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.EnableConcurrentExecution();
+
+            // TODO: Consider registering other actions that act on syntax instead of or in addition to symbols
+            // See https://github.com/dotnet/roslyn/blob/main/docs/analyzers/Analyzer%20Actions%20Semantics.md for more information
+            context.RegisterSymbolAction(AnalyzeSymbol, SymbolKind.Method);
+        }
+
+        private static void AnalyzeSymbol(SymbolAnalysisContext context)
+        {
+            var methodSymbol = (IMethodSymbol)context.Symbol;
+
+            if (MethodHasVoidReturnType(context, methodSymbol) || !MethodIsAController(context, methodSymbol) ||!MethodHasInvalidSwaggerAnnotation(context, methodSymbol))
+            {
+                return;
+            }
+
+            var diagnostic = Diagnostic.Create(
+                    descriptor: Rule,
+                    location: methodSymbol.Locations[0],
+                    messageArgs: methodSymbol.Name);
+
+            context.ReportDiagnostic(diagnostic);
+        }
+
+        private static bool MethodIsAController(SymbolAnalysisContext context, IMethodSymbol methodSymbol)
+        {
+            return MethodHasControllerAttributes(context, methodSymbol) && InheritsFromController(context, methodSymbol);
+        }
+
+        private static bool MethodHasControllerAttributes(SymbolAnalysisContext context, IMethodSymbol methodSymbol)
+        {
+            var httpMethodAttributeSymbol = context.Compilation.GetTypeByMetadataName("Microsoft.AspNetCore.Mvc.Routing.HttpMethodAttribute");
+            return methodSymbol.GetAttributes().Any(a => InheritsFromSymbol(a.AttributeClass, httpMethodAttributeSymbol));
+        }
+
+        private static bool InheritsFromController(SymbolAnalysisContext context, IMethodSymbol methodSymbol)
+        {
+            var controllerSymbol = context.Compilation.GetTypeByMetadataName("Microsoft.AspNetCore.Mvc.Controller");
+            return InheritsFromSymbol(methodSymbol.ContainingType, controllerSymbol);
+        }
+
+        private static bool InheritsFromSymbol(INamedTypeSymbol current, INamedTypeSymbol inheritedSymbol)
+        {
+            if(current == null || inheritedSymbol == null)
+            {
+                return false;
+            }
+
+            return SymbolEqualityComparer.Default.Equals(current, inheritedSymbol) || InheritsFromSymbol(current.BaseType, inheritedSymbol);
+        }
+
+        private static bool MethodHasVoidReturnType(SymbolAnalysisContext context, IMethodSymbol methodSymbol)
+        {
+            var taskSymbol = context.Compilation.GetTypeByMetadataName("System.Threading.Tasks.Task");
+            return methodSymbol.ReturnsVoid || SymbolEqualityComparer.Default.Equals(methodSymbol.ReturnType, taskSymbol);
+        }
+
+        private static bool MethodHasInvalidSwaggerAnnotation(SymbolAnalysisContext context, IMethodSymbol methodSymbol)
+        {
+            var producesResponseTypeAttributeSymbol = context.Compilation.GetTypeByMetadataName("Microsoft.AspNetCore.Mvc.ProducesResponseTypeAttribute");
+
+            var attributes = methodSymbol
+                .GetAttributes()
+                .Where(a => SymbolEqualityComparer.Default.Equals(a.AttributeClass, producesResponseTypeAttributeSymbol))
+                .ToList();
+
+            if (!attributes.Any())
+            {
+                return true;
+            }
+
+            return attributes.All(attribute => ConvertTypeNameToSwaggerTypeName(methodSymbol.ReturnType.ToDisplayString()) != GetSwaggerType(attribute));
+        }
+
+        private static string GetSwaggerType(AttributeData attribute)
+        {
+            if (attribute.ConstructorArguments.Any())
+            {
+                foreach (var arg in attribute.ConstructorArguments)
+                {
+                    if (arg.Kind == TypedConstantKind.Type)
+                    {
+                        return arg.Value.ToString();
+                    }
+                }
+            }
+
+            return attribute.NamedArguments.First(arg => arg.Key == "Type").Value.Value.ToString();
+        }
+
+        public static string ConvertTypeNameToSwaggerTypeName(string typeName)
+        {
+            // I hate this!!! It works for most cases but is a bad pattern.
+            return RemoveGenericFromTypeName(RemoveGenericFromTypeName(typeName, "Task"), "System.Threading.Tasks.Task");
+        }
+
+        private static string RemoveGenericFromTypeName(string typeName, string genericName)
+        {
+            var name = $"{genericName}<";
+            return typeName.StartsWith(name) ? typeName.Substring(name.Length, typeName.Length - 1 - name.Length) : typeName;
+        }
+    }
+}
